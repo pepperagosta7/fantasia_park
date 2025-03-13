82fae878d2600cc28d6264749e7465666570175f
pepperagosta7 <giusepperagosta29@gmail.com>
2025-03-13 16:08:02 +0100

    first commit

---
+++ src/main/java/com/example/fantasia_park/FantasiaParkApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasia_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasiaParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FantasiaParkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fantasia_park/FantasiaParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fantasia_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasiaParkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
